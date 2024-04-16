129d0a1b8770233441fff37b44318b958cd02c73
jumael99 <jumael99@gmail.com>
2024-04-16 22:29:45 +0600

    name changed

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const mongoDBUri =
     "mongodb+srv://ehtesamul99:55555@cluster0.ogknxls.mongodb.net/xy?retryWrites=true&w=majority&appName=Cluster0";
 
 const index = express();
-const PORT = process.env.PORT || 5000;
 
 index.set('view engine', 'ejs');
 
@@ -20,8 +19,8 @@ mongoose
         console.log("MongoDB connection successful");
 
         // Start the server after a successful database connection
-        index.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
+        index.listen(3000, () => {
+            console.log(`Server running on port ${3000}`);
         });
     })
     .catch((err) => {
